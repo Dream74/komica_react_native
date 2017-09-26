ce0ea7d6330c783218c8381e0e0f12070c9b7727
Dream <dream74.chang@gmail.com>
2017-09-26 15:30:40 +0800

    add eslint and use Airbnb style

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "globals": {
+        "fetch": true
+    },
+    "rules": {
+      // `.jsx` extension cannot be used with React Native
+      // https://github.com/airbnb/javascript/issues/982
+      "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+      "react/forbid-prop-types": "off",
+      'camelcase': 'off',
+    }
+};
