ed9230f559e657aee3daa03c986bca159a07f64b
Dream <dream74.chang@gmail.com>
2017-09-30 00:36:01 +0800

    The bar be hidden when forward page or next page not found,

--- src/components/Board.js
+++ src/components/Board.js
@@ -64,6 +64,67 @@ const injectJSCode = `
     }());
 `;
 
+const SwitchPage = ({ baseUrl, forwardUrl, nextUrl, onPress }) => {
+  if (forwardUrl === undefined && nextUrl === undefined) {
+    return null;
+  }
+
+  return (
+    <View style={{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 10,
+    }}
+    >
+      <TouchableOpacity
+        style={{
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+        onPress={() => {
+          if (forwardUrl !== undefined) {
+            console.log(forwardUrl);
+            if (onPress) onPress(baseUrl + forwardUrl);
+          }
+        }}
+        disabled={!forwardUrl}
+      >
+        <Text style={{ color: (!forwardUrl) ? '#3c4b63' : 'white', fontSize: 25 }}>上一頁</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+        onPress={() => {
+          if (nextUrl !== undefined) {
+            console.log(forwardUrl);
+            if (onPress) onPress(baseUrl + nextUrl);
+          }
+        }}
+        disabled={!nextUrl}
+      >
+        <Text style={{ color: (!nextUrl) ? '#3c4b63' : 'white', fontSize: 25 }}>下一頁</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+SwitchPage.propTypes = {
+  baseUrl: PropTypes.string,
+  forwardUrl: PropTypes.string,
+  nextUrl: PropTypes.string,
+  onPress: PropTypes.func,
+};
+
+SwitchPage.defaultProps = {
+  baseUrl: '',
+  forwardUrl: undefined,
+  nextUrl: undefined,
+  onPress: undefined,
+};
+
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -307,44 +368,12 @@ a:link, .qlink, .text-button {
           domStorageEnabled
           javaScriptEnabled
         />
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          padding: 10,
-        }}
-        >
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignContent: 'center',
-            }}
-            onPress={() => {
-              if (forwardUrl !== undefined) {
-                console.log(forwardUrl);
-                this.parseHTML(url + forwardUrl);
-              }
-            }}
-            disabled={!forwardUrl}
-          >
-            <Text style={{ color: (!forwardUrl) ? '#3c4b63' : 'white', fontSize: 25 }}>上一頁</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={{
-              justifyContent: 'center',
-              alignContent: 'center',
-            }}
-            onPress={() => {
-              if (nextUrl !== undefined) {
-                console.log(forwardUrl);
-                this.parseHTML(url + nextUrl);
-              }
-            }}
-            disabled={!nextUrl}
-          >
-            <Text style={{ color: (!nextUrl) ? '#3c4b63' : 'white', fontSize: 25 }}>下一頁</Text>
-          </TouchableOpacity>
-        </View>
+        <SwitchPage
+          baseUrl={url}
+          forwardUrl={forwardUrl}
+          nextUrl={nextUrl}
+          onPress={(newUrl) => { this.parseHTML(newUrl); }}
+        />
         <AdMobBanner
           bannerSize="fullBanner"
           testDeviceID="EMULATOR"
