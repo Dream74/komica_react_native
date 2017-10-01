307d6724a53b5739948460f436d0148c631fcd0e
Dream <dream74.chang@gmail.com>
2017-10-01 17:40:23 +0800

    Support external link (ex. Youtube, ...)

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -5,14 +6,11 @@ import {
   View,
   Text,
   Button,
-  TouchableOpacity,
 } from 'react-native';
 
+import urlLib from 'url';
 import {
   AdMobBanner,
-  AdMobInterstitial,
-  PublisherBanner,
-  AdMobRewarded,
 } from 'react-native-admob';
 import cio from 'cheerio-without-node-native';
 
@@ -36,19 +34,27 @@ export default class Board extends React.Component {
     this.state = {
       loading: false,
       fail: false,
+      url: '',
       html: '',
       goback: false,
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     const { url } = this.props;
+    this.setState({
+      url,
+    });
+  }
+
+  componentDidMount() {
+    const { url } = this.state;
     this.parseHTML(url);
   }
 
   componentWillReceiveProps(nextProps) {
     const newUrl = nextProps.url;
-    const oldUrl = this.props.url;
+    const oldUrl = this.state.url;
 
     if (newUrl !== oldUrl) {
       this.setState({
@@ -61,6 +67,7 @@ export default class Board extends React.Component {
   parseHTML(url) {
     console.log('parseHTML', url);
     this.setState({
+      url,
       loading: true,
       fail: false,
       html: '',
@@ -80,6 +87,124 @@ export default class Board extends React.Component {
       return;
     }
 
+    const nextHostname = urlLib.parse(url).hostname;
+    const isKomicaWebSite = komica_board.some((board_category) => {
+      const { data } = board_category;
+      return data.some((e) => {
+        const board_url = e.url;
+        return urlLib.parse(board_url).hostname === nextHostname;
+      });
+    });
+    if (isKomicaWebSite) {
+      console.log('Url', url, 'is Komica Web Site.');
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error(res.statusText);
+          }
+
+          return res;
+        })
+        .then(res => res.text())
+        .then((body) => {
+          console.log('parseHTML before');
+          const $ = cio.load(body);
+
+          const forwardUrl = $('#page_switch > table > tr > td:nth-child(1) > form').attr('action') || $('.page_switch > div.ul > div.forward > a').attr('href');
+          const nextUrl = $('#page_switch > table > tr > td:nth-child(3) > form').attr('action') || $('.page_switch > div.ul > div.next > a').attr('href');
+
+          /*
+            header: 看板描述
+            toplink: 
+            postform: 發文
+            del: 檢舉/刪除文章的功能
+            footer: 版權、統計人數
+            page_switch: 切換分頁
+            topiclist:標題一覽
+            center: 廣告
+            top: 分隔線
+            input: 刪除文章的選項
+            -del-button: 刪除文章按鈕
+            rlink: 回應文章按鈕
+            script:not([src*='common']: 所有非必要的 JS, ex: GA, ...
+            hr:nth-last-child(-n+2): 塞廣告的分割線
+          */
+
+          // remove un-used areas.
+          let ruleBeRemoved = '#header,#toplink,#postform,#del,#footer,#topiclist,center,.top,input,.-del-button';
+          // switch page
+          ruleBeRemoved += ',#page_switch,.page_switch';
+          // Monitor Javascript
+          ruleBeRemoved += ",script:not([src*='common'])";
+          // ugly line
+          ruleBeRemoved += ',hr:nth-last-child(-n+2)';
+          // it only be used in ads.
+          ruleBeRemoved += ',style,.ads_right';
+
+          // something...
+          ruleBeRemoved += ',.qlink,.report_btn';
+
+          $(ruleBeRemoved).remove();
+
+          $('body').append(`<style type="text/css">
+  html {
+      background: #1D1F21;
+      color: #C5C8C6;
+  }
+  
+  .reply {
+      background: #282A2E;
+  }
+  
+  .title {
+      color: #B294BB;
+  }
+  
+  .id, .id3 {
+      color: rgba(255,255,21,0.8);
+  }
+  
+  textarea {
+      background-color: #CDCDCD;
+  }
+  
+  a:link, .qlink, .text-button {
+      color: #81A2BE;
+      text-decoration: none;
+  }
+  
+  .post:target { background: #181818; } 
+  .popup { background: #282A2E; }
+  </style>
+  <style type="text/css">
+  `);
+          return {
+            html: $.html(),
+            forwardUrl,
+            nextUrl,
+          };
+        })
+        .then(({ html, forwardUrl, nextUrl }) => {
+          console.log('parseHTML after');
+          this.setState({
+            html,
+            forwardUrl,
+            nextUrl,
+            fail: false,
+            loading: false,
+          });
+        })
+        .catch((e) => {
+          console.log('fetch error', e);
+          this.setState({
+            html: '',
+            fail: true,
+            loading: false,
+          });
+        });
+      return;
+    }
+
     console.log('Url', url, 'is Web.');
     fetch(url)
       .then((res) => {
@@ -90,84 +215,11 @@ export default class Board extends React.Component {
         return res;
       })
       .then(res => res.text())
-      .then((body) => {
-        console.log('parseHTML before');
-        const $ = cio.load(body);
-
-        const forwardUrl = $('#page_switch > table > tr > td:nth-child(1) > form').attr('action') || $('.page_switch > div.ul > div.forward > a').attr('href');
-        const nextUrl = $('#page_switch > table > tr > td:nth-child(3) > form').attr('action') || $('.page_switch > div.ul > div.next > a').attr('href');
-
-        /*
-          header: 看板描述
-          toplink: 
-          postform: 發文
-          del: 檢舉/刪除文章的功能
-          footer: 版權、統計人數
-          page_switch: 切換分頁
-          topiclist:標題一覽
-          center: 廣告
-          top: 分隔線
-          input: 刪除文章的選項
-          -del-button: 刪除文章按鈕
-          rlink: 回應文章按鈕
-          script:not([src*='common']: 所有非必要的 JS, ex: GA, ...
-          hr:nth-last-child(-n+2): 塞廣告的分割線
-        */
-
-        // remove un-used areas.
-        let ruleBeRemoved = '#header,#toplink,#postform,#del,#footer,#topiclist,center,.top,input,.-del-button';
-        // switch page
-        ruleBeRemoved += ',#page_switch,.page_switch';
-        // Monitor Javascript
-        ruleBeRemoved += ",script:not([src*='common'])";
-        // ugly line
-        ruleBeRemoved += ',hr:nth-last-child(-n+2)';
-        // it only be used in ads.
-        ruleBeRemoved += ',style,.ads_right';
-
-        // something...
-        ruleBeRemoved += ',.qlink,.report_btn';
-
-        $(ruleBeRemoved).remove();
-
-        $('body').append(`<style type="text/css">
-html {
-    background: #1D1F21;
-    color: #C5C8C6;
-}
-
-.reply {
-    background: #282A2E;
-}
-
-.title {
-    color: #B294BB;
-}
-
-.id, .id3 {
-    color: rgba(255,255,21,0.8);
-}
-
-textarea {
-    background-color: #CDCDCD;
-}
-
-a:link, .qlink, .text-button {
-    color: #81A2BE;
-    text-decoration: none;
-}
-
-.post:target { background: #181818; } 
-.popup { background: #282A2E; }
-</style>
-<style type="text/css">
-`);
-        return {
-          html: $.html(),
-          forwardUrl,
-          nextUrl,
-        };
-      })
+      .then(body => ({
+        html: body,
+        forwardUrl: undefined,
+        nextUrl: undefined,
+      }))
       .then(({ html, forwardUrl, nextUrl }) => {
         console.log('parseHTML after');
         this.setState({
@@ -194,14 +246,14 @@ a:link, .qlink, .text-button {
   }
 
   render() {
-    const { url } = this.props;
-    const { html, loading, fail, forwardUrl, nextUrl, goback } = this.state;
+    const rootUrl = this.props.url;
+    const { url, html, loading, fail, forwardUrl, nextUrl, goback } = this.state;
 
     const WebViewGobackView =
       (goback) ? (<WebViewGoback
         onPress={() => {
           this.setState({ goback: false });
-          this.parseHTML(url);
+          this.parseHTML(rootUrl);
         }}
       />) : null;
 
@@ -300,7 +352,7 @@ a:link, .qlink, .text-button {
         />
         {WebViewGobackView}
         <SwitchPage
-          baseUrl={url}
+          baseUrl={rootUrl}
           forwardUrl={forwardUrl}
           nextUrl={nextUrl}
           onPress={(newUrl) => { this.parseHTML(newUrl); }}
